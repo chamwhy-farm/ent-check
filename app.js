ff9d2e49a79e1ac798a54e5de634ca016192ba3b
sjufkaus <sjufkaus@gmail.com>
2020-01-24 13:50:45 +0900

    update app.js

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.all('/*', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	next();
+});
+
 var scripts = {
   "1.0":fs.readFileSync('like.html', 'utf-8')
 }
