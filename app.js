65f4064de1b8a6192167b6012ac0b1392375e886
sjufkaus <sjufkaus@gmail.com>
2020-01-24 13:45:45 +0900

    update

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+var scripts = {
+  "1.0":fs.readFileSync('like.html', 'utf-8')
+}
 app.get('/', function(req, res){
  fs.readFile('like.html', 'utf-8', function(err, data){
    res.send(data);
  });
+});
+
+app.get('/install', function(req, res){
+  res.send(scripts["1.0"]);
 });
 
 app.listen(process.env.PORT);
